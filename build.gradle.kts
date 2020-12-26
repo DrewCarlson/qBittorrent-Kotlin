8a002a3b70d3a7c21b796daa8cb017df6154a612
Andrew Carlson <rotexdev@gmail.com>
2020-12-25 22:23:46 -0800

    Fix windows publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,24 @@
 plugins {
-  kotlin("multiplatform") version KOTLIN_VERSION apply false
-  kotlin("plugin.serialization") version KOTLIN_VERSION apply false
+    kotlin("multiplatform") version KOTLIN_VERSION apply false
+    kotlin("plugin.serialization") version KOTLIN_VERSION apply false
 }
 
 buildscript {
-  repositories {
-    gradlePluginPortal()
-    mavenCentral()
-  }
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
 }
 
 allprojects {
-  repositories {
-    mavenCentral()
-    jcenter()
-  }
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
 }
 
+System.getenv("GITHUB_REF")?.let { ref ->
+    if (ref.contains("tag")) {
+        version = (version as String).substringBefore("-SNAPSHOT")
+    }
+}
