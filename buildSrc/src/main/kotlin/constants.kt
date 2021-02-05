12afbff679b2938813daad0e5c512da01ab2c427
Drew Carlson <DrewCarlson@users.noreply.github.com>
2021-02-04 21:56:19 -0800

    Update dependencies

--- buildSrc/src/main/kotlin/constants.kt
+++ buildSrc/src/main/kotlin/constants.kt
@@ -1,4 +1,4 @@
-const val KOTLIN_VERSION = "1.4.21"
-const val KTOR_VERSION = "1.5.0"
+const val KOTLIN_VERSION = "1.4.30"
+const val KTOR_VERSION = "1.5.1"
 const val COROUTINES_VERSION = "1.4.2-native-mt"
 const val SERIALIZATION_VERSION = "1.0.1"
