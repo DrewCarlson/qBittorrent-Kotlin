dd50daca088dc356c3a1e11e5b08365ee72879e3
shalva97 <shalva258@gmail.com>
2021-03-08 19:48:44 +0400

    update names

--- demo/src/jvmMain/java/main.kt
+++ demo/src/jvmMain/java/main.kt
@@ -1,12 +1,26 @@
 package demo
 
 import drewcarlson.qbittorrent.QBittorrentClient
+import io.ktor.client.*
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
+import java.net.InetSocketAddress
+import java.net.Proxy
 
 fun main(vararg args: String) = runBlocking {
     val client = QBittorrentClient(
-        baseUrl = args.firstOrNull() ?: "http://localhost:9090"
+        baseUrl = "http://192.168.88.6:8080",
+        httpClient = HttpClient {
+            engine {
+                proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress(8888))
+            }
+        }
     )
+
+    client.syncMainData()
+        .collect {
+
+        }
 
     println("qBittorrent Version: ${client.getVersion()}")
 }
