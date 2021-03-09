9430093a790f338585e39ca1b1421b0aee98e5f5
shalva97 <shalva258@gmail.com>
2021-03-10 03:39:18 +0400

    Revert "update names"
    This reverts commit dd50daca

--- demo/src/jvmMain/java/main.kt
+++ demo/src/jvmMain/java/main.kt
@@ -1,26 +1,12 @@
 package demo
 
 import drewcarlson.qbittorrent.QBittorrentClient
-import io.ktor.client.*
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
-import java.net.InetSocketAddress
-import java.net.Proxy
 
 fun main(vararg args: String) = runBlocking {
     val client = QBittorrentClient(
-        baseUrl = "http://192.168.88.6:8080",
-        httpClient = HttpClient {
-            engine {
-                proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress(8888))
-            }
-        }
+        baseUrl = args.firstOrNull() ?: "http://localhost:9090"
     )
-
-    client.syncMainData()
-        .collect {
-
-        }
 
     println("qBittorrent Version: ${client.getVersion()}")
 }
