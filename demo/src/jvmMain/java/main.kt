0c22f22c6139e8b663a22f5789c240d166fa822b
shalva97 <shalva258@gmail.com>
2021-03-10 03:54:21 +0400

    print some fields from maindata

--- demo/src/jvmMain/java/main.kt
+++ demo/src/jvmMain/java/main.kt
@@ -1,12 +1,26 @@
 package demo
 
 import drewcarlson.qbittorrent.QBittorrentClient
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 
 fun main(vararg args: String) = runBlocking {
     val client = QBittorrentClient(
-        baseUrl = args.firstOrNull() ?: "http://localhost:9090"
+        baseUrl = args.firstOrNull() ?: "http://192.168.88.6:8080"
     )
 
     println("qBittorrent Version: ${client.getVersion()}")
+
+    client.syncMainData()
+        .collect {
+            val totalNumberOfTorrents = it.torrents.size
+            val currentUploadSpeed = it.serverState?.upInfoSpeed
+            val currentDownloadSpeed = it.serverState?.dlInfoSpeed
+            val allTimeDownload = it.serverState?.allTimeDownload
+            val allTimeUpload = it.serverState?.allTimeUpload
+
+            println("there are $totalNumberOfTorrents torrents in total with upload speed of $currentUploadSpeed and download speed of $currentDownloadSpeed")
+            println("you have all-time upload of $allTimeUpload and all-time download of $allTimeDownload")
+            println()
+        }
 }
