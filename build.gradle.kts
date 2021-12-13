72583d5c873703281b06810ff65e2dfd135cc9e1
Drew Carlson <awcarlson@protonmail.com>
2021-12-12 18:57:39 -0800

    Update dokka version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform") version KOTLIN_VERSION apply false
     kotlin("plugin.serialization") version KOTLIN_VERSION apply false
-    id("org.jetbrains.dokka") version "1.4.20" apply false
+    id("org.jetbrains.dokka") version DOKKA_VERSION
 }
 
 allprojects {
