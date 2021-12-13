74df0d8c45b6bf42c3f22670f2ea87945d442e39
Drew Carlson <awcarlson@protonmail.com>
2021-12-13 10:51:51 -0800

    Remove native object freezing with new MM

--- client/src/nativeCommonMain/kotlin/Atomics.kt
+++ client/src/nativeCommonMain/kotlin/Atomics.kt
@@ -4,11 +4,15 @@ import kotlin.native.concurrent.freeze
 
 internal actual class AtomicReference<T> actual constructor(value: T) {
 
-    private val ref = kotlin.native.concurrent.AtomicReference(value.freeze())
+    private val ref = kotlin.native.concurrent.AtomicReference(value.maybeFreeze())
 
     actual var value: T
         get() = ref.value
         set(value) {
-            ref.value = value.freeze()
+            ref.value = value.maybeFreeze()
         }
+}
+
+private fun  <T> T.maybeFreeze(): T {
+    return if (Platform.memoryModel == MemoryModel.EXPERIMENTAL) this else freeze()
 }
