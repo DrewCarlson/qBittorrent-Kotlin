cdfad29583d8c54c070c0b0ea80937a386f7c95d
Drew Carlson <awcarlson@protonmail.com>
2022-07-12 16:11:51 -0700

    Return logging to demo http client

--- demo/src/commonMain/kotlin/program.kt
+++ demo/src/commonMain/kotlin/program.kt
@@ -1,11 +1,19 @@
 package demo
 
+import io.ktor.client.*
+import io.ktor.client.plugins.logging.*
 import qbittorrent.*
 import kotlinx.coroutines.flow.*
 
 suspend fun runProgram(args: Array<out String>) {
     val client = QBittorrentClient(
-        baseUrl = args.firstOrNull() ?: "http://localhost:9090"
+        baseUrl = args.firstOrNull() ?: "http://localhost:9090",
+        httpClient = HttpClient {
+            Logging {
+                level = LogLevel.ALL
+                logger = Logger.SIMPLE
+            }
+        }
     )
 
     println("qBittorrent Version: ${client.getVersion()}")
