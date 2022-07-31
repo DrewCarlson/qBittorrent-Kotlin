193a4a14d92973d5666c3d6bc9d1ac601834b436
Drew Carlson <awcarlson@protonmail.com>
2022-07-30 17:36:00 -0700

    Add `observeTorrentPeers` for the `/sync/torrentPeers?hash=` endpoint
    - Refactor MainDataSync into reusable `DataSync<T>` utility

--- client/src/commonMain/kotlin/internal/HttpUtils.kt
+++ client/src/commonMain/kotlin/internal/HttpUtils.kt
@@ -3,11 +3,13 @@ package qbittorrent.internal
 import io.ktor.client.call.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.util.reflect.*
 import qbittorrent.QBittorrentException
 
 internal suspend fun HttpResponse.orThrow() {
     if (!status.isSuccess()) {
-        throw call.attributes.takeOrNull(ErrorTransformer.KEY_INTERNAL_ERROR)?.run(::QBittorrentException)
+        throw call.attributes.takeOrNull(ErrorTransformer.KEY_INTERNAL_ERROR)
+            ?.run(::QBittorrentException)
             ?: QBittorrentException(this, bodyAsText())
     }
 }
@@ -19,7 +21,18 @@ internal suspend inline fun <reified T> HttpResponse.bodyOrThrow(): T {
             else -> body()
         }
     } else {
-        throw call.attributes.takeOrNull(ErrorTransformer.KEY_INTERNAL_ERROR)?.run(::QBittorrentException)
+        throw call.attributes.takeOrNull(ErrorTransformer.KEY_INTERNAL_ERROR)
+            ?.run(::QBittorrentException)
+            ?: QBittorrentException(this, bodyAsText())
+    }
+}
+
+internal suspend fun <T> HttpResponse.bodyOrThrow(typeInfo: TypeInfo): T {
+    return if (status.isSuccess()) {
+        body(typeInfo)
+    } else {
+        throw call.attributes.takeOrNull(ErrorTransformer.KEY_INTERNAL_ERROR)
+            ?.run(::QBittorrentException)
             ?: QBittorrentException(this, bodyAsText())
     }
 }
